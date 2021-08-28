b3bc664763725106cae123596835f867ad375f3d
antonio-arcanjo <tonyassister@gmail.com>
2021-08-28 18:58:16 -0300

    Projeto Banco 1

---
+++ Classes/Conta.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace Projeto
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+
+        private double Saldo { get; set; }
+
+        private double Credito { get; set; }
+
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+
+
+        public bool Sacar(double valorSaque)
+        {
+            //validação de saldo suficiente
+            if (this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                Console.WriteLine("saldo insuficiente");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+            //https://docs.microsoft.com/pt-br/dotnet/standard/base-types/composite-formatting
+
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "TipoConta " + this.TipoConta + " | ";
+            retorno += "Nome " + this.Nome + " | ";
+            retorno += "Saldo " + this.Saldo + " | ";
+            retorno += "Credito " + this.Credito;
+            return retorno;
+        }
+
+
+
+    }
+
+
+}
---
+++ Enum/TipoConta.cs
@@ -0,0 +1,10 @@
+namespace Projeto{
+    
+    public enum TipoConta
+    {
+       PessoaFisica = 1,
+
+       PessoaJuridica = 2
+        
+    }
+}
